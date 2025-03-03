5e5d938b352771c50e029d6c04ebfff1d43eaaa2
Nipun Shrivats <nipunrawat16@gmail.com>
2025-03-03 23:16:10 +0530

    new updates

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+import Hero from "./Hero.jpg";
+import logo from "./logo.png";
+import img1 from "./img1.png";
+import img2 from "./img2.png";
+import img3 from "./img3.png";
+
+export { Hero, logo, img1, img2, img3 };
