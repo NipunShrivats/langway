e873390c5d73827bffed8acc0427be43e18c848e
Nipun Shrivats <nipunrawat16@gmail.com>
2025-03-03 22:51:03 +0530

    changes 6

--- src/Data.js
+++ src/Data.js
@@ -4,7 +4,7 @@ export const Data = [
     course_name: "Personalized Learning Styles",
     subtitle: "sub1",
     description: `Discover your unique learning preferences—whether you learn best through visuals, listening, reading/writing, or hands-on activities, we adapt our courses to fit your style.`,
-    image: "src/assets/images/img1.png",
+    image: "./src/assets/images/img1.png",
   },
   {
     id: 2,
@@ -12,13 +12,13 @@ export const Data = [
     subtitle: "sub2",
     description: `Accurately gauge your current English proficiency level with our online tests, ensuring you start at the right place for your learning journey.
 `,
-    image: "src/assets/images/img2.png",
+    image: "./src/assets/images/img2.png",
   },
   {
     id: 3,
     course_name: "Budget-Friendly Options",
     subtitle: "sub3",
     description: `Filter courses by your budget to find the best value for your investment in language learning without compromising on quality.`,
-    image: "src/assets/images/img3.png",
+    image: "./src/assets/images/img3.png",
   },
 ];
