5079faef3ccfd3dff58b8bc129da00ee1abe34c2
Nipun Shrivats <nipunrawat16@gmail.com>
2025-03-04 11:20:24 +0530

    updates on 4-3-25, change no. 2

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,27 +6,34 @@ export default function Footer() {
   return (
     <>
       <div className="footer">
-        <Link
-          activeClass="active"
-          to="cards"
-          spy={true}
-          smooth={true}
-          offset={-150}
-          duration={500}
-        >
-          <button>Explore Courses</button>
-        </Link>
+        <div className="footer_btns">
+          <Link
+            activeClass="active"
+            to="cards"
+            spy={true}
+            smooth={true}
+            offset={-150}
+            duration={500}
+          >
+            <button>Explore Courses</button>
+          </Link>
 
-        <Link
-          activeClass="active"
-          to="cards"
-          spy={true}
-          smooth={true}
-          offset={-1000}
-          duration={500}
-        >
-          <button>Go to the top</button>
-        </Link>
+          <Link
+            activeClass="active"
+            to="cards"
+            spy={true}
+            smooth={true}
+            offset={-1000}
+            duration={500}
+          >
+            <button>Go to the top</button>
+          </Link>
+        </div>
+        <div className="copyright-tab">
+          <p className="copyright">
+            © 2025, All rights reserved | <span>LangWay</span>
+          </p>
+        </div>
       </div>
     </>
   );
